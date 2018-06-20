3dd7c916ef94f311eecccc9e35b676ecdcc2ef8a
kpostlet <kpostlet@redhat.com>
2018-06-20 15:26:15 -0400

    Dynamic regex creation added

--- config.py
+++ config.py
@@ -7,3 +7,49 @@ dbName = "QE_Database"
 client = MongoClient(mongoURI, mongoPort)
 db = client[dbName]
 collection=db[collectionName]
+
+validPdfName = '^\w+\.pdf$'
+
+chartStructure = 'charts.json'
+
+with open(chartStructure) as file:
+    structure = json.load(file)
+
+def cardsRegex():
+    reg = "^("
+    for card in structure['cards']:
+        reg += card + "|"
+    reg = reg[:-1]
+    reg = reg + ")$"
+    return reg
+
+def testsRegex():
+    reg = "^("
+    for test in structure['tests']:
+        reg += test + "|"
+    reg = reg[:-1]
+    reg = reg + ")$"
+    return reg
+
+def subTestsRegex(test):
+    reg = "^("
+    for subTest in structure['tests'][test]:
+        reg += subTest + "|"
+    reg = reg[:-1]
+    reg = reg + ")$"
+    return reg
+
+def typeRegex(test,subTest):
+    reg = "^("
+    for type in structure['tests'][test][subTest]:
+        reg += type + "|"
+    reg = reg[:-1]
+    reg = reg + ")$"
+    return reg
+
+def labelsRegex(test, subTest, type):
+    reg = "^("
+    for label in structure['tests'][test][subTest][type]:
+        reg += label + "|"
+    reg = reg[:-1]
+    reg = reg + ")$"
