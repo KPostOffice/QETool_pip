4035407a58e850bafdb121956975e787e7bdf8ce
kpostlet <kpostlet@redhat.com>
2018-07-23 11:09:02 -0400

    Added dependencies to setup.py file

--- graphMaker/setup.py
+++ graphMaker/setup.py
@@ -18,6 +18,10 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
+    install_requires=[
+      'requests',
+      'matplotlib',
+    ],
     entry_points = {
         'console_scripts': [
             'fileMaker=pythonFiles.createFile:main'
